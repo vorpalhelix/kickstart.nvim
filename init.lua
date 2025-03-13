f70ea21df1f0ab28221e3c73925c7399aeba3b11
vorpalhelix <73440575+vorpalhelix@users.noreply.github.com>
2025-03-13 17:14:12 +0530

    remove braces

--- init.lua
+++ init.lua
@@ -988,8 +988,8 @@ require('lazy').setup({
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
-}, {
+  -- you can continue same window with `<space>sr` which resumes last telescope search 
+  {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
