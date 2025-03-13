e629d298140a0cb8b322e72a43af597b3b97b6fa
vorpalhelix <73440575+vorpalhelix@users.noreply.github.com>
2025-03-13 17:12:15 +0530

    remove comma

--- init.lua
+++ init.lua
@@ -871,7 +871,7 @@ require('lazy').setup({
   { 
     'bluz71/vim-moonfly-colors', name = "moonfly", lazy = false, priority = 999, 
     config = function()
-      vim.cmd.colorscheme 'moonfly',
+      vim.cmd.colorscheme 'moonfly'
     end,
   },
 
